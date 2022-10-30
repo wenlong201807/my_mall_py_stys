c010799b9dec1d9699fbb1332fbfb373c8661ab8
dragon <1573511441@qq.com>
2022-10-30 15:58:32 +0800

    feat: 接口添加访问权限

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views.py
@@ -9,7 +9,7 @@ from rest_framework.permissions import IsAdminUser
 # 1, 获取用户总数
 class UserTotalCountView(APIView):
     # 1, 设置管理员权限
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAdminUser]  # 局部添加接口访问权限
 
     def get(self, request):
         # 1, 查询用户总数 且过滤掉管理员用户 .filter(is_staff=False)
@@ -23,6 +23,9 @@ class UserTotalCountView(APIView):
 
 # 2, 获取日活用户
 class UserDayActiveView(APIView):
+    # 1, 设置管理员权限
+    permission_classes = [IsAdminUser]  # 局部添加接口访问权限
+
     def get(self, request):
         # 1, 查询用户日活数量
         count = User.objects.filter(last_login__gte=date.today(), is_staff=False).count()
@@ -36,7 +39,7 @@ class UserDayActiveView(APIView):
 # 3, 获取日增用户
 class UserDayIncrementView(APIView):
     # 1, 设置管理员权限
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAdminUser]  # 局部添加接口访问权限
 
     def get(self, request):
         # 1, 查询用户日增数量, 注意点: date.today() 获取的不带时分秒
@@ -50,6 +53,9 @@ class UserDayIncrementView(APIView):
 
 # 4, 获取日下单用户
 class UserDayOrdersView(APIView):
+    # 1, 设置管理员权限
+    permission_classes = [IsAdminUser]  # 局部添加接口访问权限
+
     def get(self, request):
         # 1, 查询用户日下单用户数量
         count = User.objects.filter(orderinfo__create_time__gte=date.today(), is_staff=False).count()
