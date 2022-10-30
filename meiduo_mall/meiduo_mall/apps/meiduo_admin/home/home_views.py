f4646fc2adb40348285dcf2a2d59bcbf83a166d3
dragon <1573511441@qq.com>
2022-10-30 19:23:28 +0800

    refactory: 按照前端菜单模块 将后台文件夹分模块管理

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/home/home_views.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/home/home_views.py
@@ -5,10 +5,75 @@ from users.models import User
 from datetime import date, timedelta
 from rest_framework.permissions import IsAdminUser
 from goods.models import CategoryVisitCount
+from orders.models import OrderInfo
 from . import home_serializers
 
 
-# 1, 获取月增用户
+# 1, 获取用户总数
+class UserTotalCountView(APIView):
+    # 1, 设置管理员权限
+    permission_classes = [IsAdminUser]  # 局部添加接口访问权限
+
+    def get(self, request):
+        # 1, 查询用户总数 且过滤掉管理员用户 .filter(is_staff=False)
+        count = User.objects.filter(is_staff=False).count()
+
+        # 2, 返回响应
+        return Response({
+            "count": count
+        })
+
+
+# 2, 获取日活用户
+class UserDayActiveView(APIView):
+    # 1, 设置管理员权限
+    permission_classes = [IsAdminUser]  # 局部添加接口访问权限
+
+    def get(self, request):
+        # 1, 查询用户日活数量
+        count = User.objects.filter(last_login__gte=date.today(), is_staff=False).count()
+
+        # 2, 返回响应
+        return Response({
+            "count": count
+        })
+
+
+# 3, 获取日增用户
+class UserDayIncrementView(APIView):
+    # 1, 设置管理员权限
+    permission_classes = [IsAdminUser]  # 局部添加接口访问权限
+
+    def get(self, request):
+        # 1, 查询用户日增数量, 注意点: date.today() 获取的不带时分秒
+        count = User.objects.filter(date_joined__gte=date.today(), is_staff=False).count()
+
+        # 2, 返回响应
+        return Response({
+            "count": count
+        })
+
+
+# 4, 获取日下单用户
+class UserDayOrdersView(APIView):
+    # 1, 设置管理员权限
+    permission_classes = [IsAdminUser]  # 局部添加接口访问权限
+
+    def get(self, request):
+        # 1, 查询用户日下单用户数量
+        count = User.objects.filter(orderinfo__create_time__gte=date.today(), is_staff=False).count()
+        # count = User.objects.filter(haha__create_time__gte=date.today(), is_staff=False).count()
+        # 方式二 [更合理] 此功能使用orderinfo表查询更恰当  requese 为当前用户数据 user_id=request.user.id
+        cot = OrderInfo.objects.filter(create_time__gte=date.today()).count()
+
+        # 2, 返回响应
+        return Response({
+            # "count": count
+            "count": cot
+        })
+
+
+# 5, 获取月增用户
 class UserMonthIncrementView(APIView):
     # 1, 设置管理员权限
     permission_classes = [IsAdminUser]
@@ -39,7 +104,7 @@ class UserMonthIncrementView(APIView):
         return Response(count_list)
 
 
-# 2, 获取商品日分类访问量
+# 6, 获取商品日分类访问量
 class GoodCategoryDayView(ListAPIView):
     """
     ListAPIView:
