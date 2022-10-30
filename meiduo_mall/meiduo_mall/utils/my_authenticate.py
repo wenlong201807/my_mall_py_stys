903e837a8a40b304ad2455cbc94c1b29a3e18e11
dragon <1573511441@qq.com>
2022-10-30 11:54:24 +0800

    feat: 管理员账号才可以登陆后台管理系统

--- meiduo_mall/meiduo_mall/utils/my_authenticate.py
+++ meiduo_mall/meiduo_mall/utils/my_authenticate.py
@@ -5,12 +5,19 @@ from users.models import User
 #1, 重写系统认证方法
 class MyModelBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
-
-        #0, 判断request对象是否存在
-        if request is None: #管理员
+        """
+         注意【约定】： 如果是管理员用户 request 无信息，如果是普通用户，request 有信息
+        :param request: 请求体
+        :param username: 用户名
+        :param password: 密码
+        :param kwargs: params 参数
+        :return: { token, username, id }
+        """
+        # 0, 判断request对象是否存在
+        if request is None:  # 管理员
             try:
                 if re.match(r'^1[3-9]\d{9}$',username):
-                    user = User.objects.get(mobile=username,is_staff=True)
+                    user = User.objects.get(mobile=username,is_staff=True)  # is_staff 是管理员，数据表中 is_staff = 1
                 else:
                     user = User.objects.get(username=username,is_staff=True)
             except Exception as e:
@@ -22,8 +29,8 @@ class MyModelBackend(ModelBackend):
             else:
                 return None
 
-        else:
-            #1, 根据username查询用户
+        else:  # 普通用户
+            # 1, 根据username查询用户
             try:
                 if re.match(r'^1[3-9]\d{9}$',username):
                     user = User.objects.get(mobile=username)
