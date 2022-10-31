76ba3223419bbf70bbdcce8574b21d395baaaab3
dragon <1573511441@qq.com>
2022-10-31 21:28:13 +0800

    feat: 用户管理，获取数据列表+分页

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
@@ -1,14 +1,17 @@
 from django.conf.urls import url
 from rest_framework_jwt.views import obtain_jwt_token
 from meiduo_admin.home import home_views
+from meiduo_admin.user import user_views
 
 urlpatterns = [
     url(r'^authorizations/$', obtain_jwt_token),  # 内部对 用户名和密码做了校验
+    # 首页
     url(r'^statistical/total_count/$', home_views.UserTotalCountView.as_view()),
     url(r'^statistical/day_active/$', home_views.UserDayActiveView.as_view()),
     url(r'^statistical/day_increment/$', home_views.UserDayIncrementView.as_view()),
     url(r'^statistical/day_orders/$', home_views.UserDayOrdersView.as_view()),
-
     url(r'^statistical/month_increment/$', home_views.UserMonthIncrementView.as_view()),
     url(r'^statistical/goods_day_views/$', home_views.GoodCategoryDayView.as_view()),
+    # 用户管理
+    url(r'^users/$', user_views.UserView.as_view()),
 ]
